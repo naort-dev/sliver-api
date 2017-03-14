86d568a42610e12285d4a1ec9efa78ca7dee6485
VsevolodY <vsevolod@smallbizsilverlining.com>
2017-03-14 12:30:19 +0200

    test6 send mailer

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
      mailer : {
           accessKeyId: "AKIAIA62CYCWJTLMM6JQ",
           secretAccessKey: "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",
-          rateLimit: 5 // do not send more than 5 messages in a second
+          rateLimit: 5,  // do not send more than 5 messages in a second
+          region : 'us-west-2'
      }
 };
