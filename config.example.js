24accdd2a1b1b9270ecc0ae0c2c759f14e1fe177
David Torres <davidj_torres@hotmail.com>
2017-05-29 13:19:30 -0500

    Added secret to config example file

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+    host : 'http://', //host
+    db : process.env.db || '',   // mongodb localhost
+    mailer : {
+        accessKeyId: "", //AWS
+        secretAccessKey: "", //AWS
+        rateLimit: 5,  // do not send more than 5 messages in a second
+        region : '' //region AWS
+    },
+    emailAddressAdmin : '', //AWS adress
+    emailAddressSupport : '', //AWS adress
+    stripe_key : ''  // stripe key,
+    secret: ''
+};
